4d3605642f2926030a40d4efc8c4643ac63ff7f7
Vivek <vivekpatil@gmail.com>
2019-11-26 15:28:04 +0530

    added product xyz in git

--- xyz.js
+++ xyz.js
@@ -1,7 +1,9 @@
-console.log(hi);
-console.log(hlo);
+console.log(nxt);
+console.log(nxt);
 console.log(nxt);
 console.log(nxt);
 
+console.log(nxt);
+console.log(nxt);
 console.log(nxt);
 console.log(nxt);
